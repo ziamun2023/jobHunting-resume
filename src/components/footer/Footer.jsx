0f740e04411cd7aaf1c5a807519e9616a5347f2e
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-07-03 02:28:08 +0600

    updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-       <div className='bg-black w-full  h-48 text-white relative  '> <p className='text'>&copy; all rights reserved   SHopsense </p> <p className='p-4'>Contact: +9908665234 <br /> address: 11th street , roadcity , xyCountry <br /> email: shopsense334@gmai.com </p>  </div>
+       <div className='bg-black w-full  h-48 text-white relative  '> <p className='text'>&copy; all rights reserved  | SHopsense </p> <p className='p-4'>Contact: +9908665234 <br /> address: 11th street , roadcity , xyCountry <br /> email: shopsense334@gmai.com </p>  </div>
         </div>
     );
 };
